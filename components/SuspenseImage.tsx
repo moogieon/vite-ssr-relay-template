e8f817e9a51ebe24a7770021f70e2ad18360edb6
moogieon <vzlzydn5007@gmail.com>
2022-02-08 16:52:19 +0900

    commentPage 페이지네이션 추가

---
+++ components/SuspenseImage.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import JSResource from './JSResource'
+
+interface Props {
+  src: string
+  title: string
+  className: string
+}
+typeof window === 'undefined'
+const SuspenseImage: React.FC<Props> = (props) => {
+  const { src, title } = props
+  if (src != null) {
+    const resource = JSResource(src, () => {
+      return new Promise((resolve) => {
+        const img = new Image()
+        img.onload = () => {
+          resolve(src)
+        }
+        img.onerror = (error) => {
+          console.error(error)
+          resolve(src)
+        }
+        img.src = src
+      })
+    })
+    resource.load() // TODO: JSResource::read() should call load() if necessary
+    resource.read() // suspends while the image is pending
+  }
+  return <img alt={title} {...props} />
+}
+export default SuspenseImage
